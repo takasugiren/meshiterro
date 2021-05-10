c449fd7136ebfafe9af24c6f8eace8dc85a3a670
takasugiren <bucyou16528080@gmail.com>
2021-05-10 12:14:32 +0000

    [Update]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-
- root to: 'homes#top'
  devise_for :users
- resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ root to: 'homes#top'
+ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resources :favorites, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy]
+ end
 end
