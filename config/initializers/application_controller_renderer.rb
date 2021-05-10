5843c271aa2aa894dd847174e9009584eb0a50dc
takasugiren <bucyou16528080@gmail.com>
2021-05-10 08:38:23 +0000

    [Update]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4609c5fe60f7280f678422c08167388df9c277cd61c55edf2256459147e3539fe96bae995147a41ade5c2e265e954830c706d2c15dd15c1288d9ef9d2128ab9d'
